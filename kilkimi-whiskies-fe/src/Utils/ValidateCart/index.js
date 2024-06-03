cf32a5b5efa1cd5f95a96fecc761dba5c9663feb
TheSiggs <samsiggs00@gmail.com>
2024-06-03 15:13:11 +1200

    Remove util dir

--- kilkimi-whiskies-fe/src/Utils/ValidateCart/index.js
+++
@@ -1,8 +0,0 @@
-
-
-export const validateCart = (prevCart, item, cartTotal) => {
-    const existingItem = prevCart.find(cartItem => cartItem.sku === item.sku);
-    const type = ;
-    console.log(prevCart);
-    console.log(item);
-}
