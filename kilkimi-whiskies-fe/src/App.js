391ec59b97912456ef179d02721dea65cd607f10
TheSiggs <samsiggs00@gmail.com>
2024-06-03 16:32:04 +1200

    Removed unnecessary import

--- kilkimi-whiskies-fe/src/App.js
+++ kilkimi-whiskies-fe/src/App.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
-import {logDOM} from "@testing-library/react";
 import {Product} from "./components/Product";
 import {CartItem} from "./components/CartItem";
 
